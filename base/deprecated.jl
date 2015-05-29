b3275495799b79e9f4524d528e607968c8e97150
Jacob Quinn <quinn.jacobd@gmail.com>
2015-05-29 09:08:04 -0600

    mmap overhaul

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -479,3 +479,120 @@ export float32_isvalid, float64_isvalid
 # 11379
 
 @deprecate utf32(c::Integer...)   UTF32String(UInt32[c...,0])
+
+# 11280, mmap
+
+msync{T}(A::Array{T}) = msync(pointer(A), length(A)*sizeof(T))
+msync(B::BitArray) = msync(pointer(B.chunks), length(B.chunks)*sizeof(UInt64))
+
+@unix_only begin
+# Low-level routines
+# These are needed for things like MAP_ANONYMOUS
+function mmap(len::Integer, prot::Integer, flags::Integer, fd, offset::Integer)
+    depwarn("`mmap` is deprecated, use `Mmap.Array` instead", :mmap)
+    const pagesize::Int = ccall(:jl_getpagesize, Clong, ())
+    # Check that none of the computations will overflow
+    if len < 0
+        throw(ArgumentError("requested size must be ≥ 0, got $len"))
+    end
+    if len > typemax(Int)-pagesize
+        throw(ArgumentError("requested size must be ≤ $(typemax(Int)-pagesize), got $len"))
+    end
+    # Set the offset to a page boundary
+    offset_page::FileOffset = floor(Integer,offset/pagesize)*pagesize
+    len_page::Int = (offset-offset_page) + len
+    # Mmap the file
+    p = ccall(:jl_mmap, Ptr{Void}, (Ptr{Void}, Csize_t, Cint, Cint, Cint, FileOffset), C_NULL, len_page, prot, flags, fd, offset_page)
+    systemerror("memory mapping failed", reinterpret(Int,p) == -1)
+    # Also return a pointer that compensates for any adjustment in the offset
+    return p, Int(offset-offset_page)
+end
+
+function munmap(p::Ptr,len::Integer)
+    depwarn("`munmap` is deprecated, `Mmap.Array` is automatically munmapped when finalized", :munmap)
+    systemerror("munmap", ccall(:munmap,Cint,(Ptr{Void},Int),p,len) != 0)
+end
+
+const MS_ASYNC = 1
+const MS_INVALIDATE = 2
+const MS_SYNC = 4
+function msync(p::Ptr, len::Integer, flags::Integer=MS_SYNC)
+    depwarn("`msync` is deprecated, use `Mmap.sync!` instead", :msync)
+    systemerror("msync", ccall(:msync, Cint, (Ptr{Void}, Csize_t, Cint), p, len, flags) != 0)
+end
+end
+
+
+@windows_only begin
+function munmap(viewhandle::Ptr, mmaphandle::Ptr)
+    depwarn("`munmap` is deprecated, `Mmap.Array` is automatically munmapped when finalized", :munmap)
+    status = ccall(:UnmapViewOfFile, stdcall, Cint, (Ptr{Void},), viewhandle)!=0
+    status |= ccall(:CloseHandle, stdcall, Cint, (Ptr{Void},), mmaphandle)!=0
+    if !status
+        error("could not unmap view: $(FormatMessage())")
+    end
+end
+
+function msync(p::Ptr, len::Integer)
+    depwarn("`msync` is deprecated, use `Mmap.sync!` instead", :msync)
+    status = ccall(:FlushViewOfFile, stdcall, Cint, (Ptr{Void}, Csize_t), p, len)!=0
+    if !status
+        error("could not msync: $(FormatMessage())")
+    end
+end
+
+end
+
+mmap_array{T,N}(::Type{T}, dims::NTuple{N,Integer}, s::IO) = mmap_array(T, dims, s, position(s))
+
+@unix_only @deprecate mmap_array Mmap.Array
+
+@windows_only begin
+type SharedMemSpec
+    name :: AbstractString
+    readonly :: Bool
+    create :: Bool
+end
+function mmap_array{T,N}(::Type{T}, dims::NTuple{N,Integer}, s::Union(IO,SharedMemSpec), offset::FileOffset)
+    depwarn("`mmap_array` is deprecated, use `Mmap.Array` instead", :mmap_array)
+    if isa(s,SharedMemSpec)
+        a = Mmap.AnonymousMmap(s.name, s.readonly, s.create)
+    else
+        a = s
+    end
+    return Mmap.Array(T, dims, a, offset)
+end
+end
+
+mmap_bitarray{N}(::Type{Bool}, dims::NTuple{N,Integer}, s::IOStream, offset::FileOffset=position(s)) =
+    mmap_bitarray(dims, s, offset)
+function mmap_bitarray{N}(dims::NTuple{N,Integer}, s::IOStream, offset::FileOffset=position(s))
+    depwarn("`mmap_bitarray` is deprecated, use `Mmap.Array` instead", :mmap_bitarray)
+    iswrite = !isreadonly(s)
+    n = 1
+    for (i, d) in enumerate(dims)
+        if d < 0
+            throw(ArgumentError("dimension size must be ≥ 0, got $d size for dimension $i"))
+        end
+        n *= d
+    end
+    nc = num_bit_chunks(n)
+    if nc > typemax(Int)
+        throw(ArgumentError("file is too large to memory-map on this platform"))
+    end
+    chunks = Mmap.Array(UInt64, (nc,), s, offset)
+    if iswrite
+        chunks[end] &= _msk_end(n)
+    else
+        if chunks[end] != chunks[end] & _msk_end(n)
+            throw(ArgumentError("the given file does not contain a valid BitArray of size $(join(dims, 'x')) (open with \"r+\" mode to override)"))
+        end
+    end
+    B = BitArray{N}(ntuple(N,i->0)...)
+    B.chunks = chunks
+    B.len = n
+    if N != 1
+        B.dims = dims
+    end
+    return B
+end
---
+++ test/mmap.jl
@@ -0,0 +1,224 @@
+file = tempname()
+s = open(file, "w") do f
+    write(f, "Hello World\n")
+end
+t = "Hello World".data
+@test Mmap.Array(UInt8, (11,1,1), file) == reshape(t,(11,1,1))
+gc()
+@test Mmap.Array(UInt8, (1,11,1), file) == reshape(t,(1,11,1))
+gc()
+@test Mmap.Array(UInt8, (1,1,11), file) == reshape(t,(1,1,11))
+gc()
+@test_throws ArgumentError Mmap.Array(UInt8, (11,0,1), file) # 0-dimension results in len=0
+@test Mmap.Array(UInt8, (11,), file) == t
+gc()
+@test Mmap.Array(UInt8, (1,11), file) == t'
+gc()
+@test_throws ArgumentError Mmap.Array(UInt8, (0,12), file)
+m = Mmap.Array(UInt8, (1,2,1),  file)
+@test m == reshape("He".data,(1,2,1))
+m=nothing; gc()
+
+s = open(f->f,file,"w")
+@test_throws ArgumentError Mmap.Array(file) # requested len=0 on empty file
+@test_throws ArgumentError Mmap.Array(file,0)
+m = Mmap.Array(file,12)
+m[:] = "Hello World\n".data
+Mmap.sync!(m)
+m=nothing; gc()
+@test open(readall,file) == "Hello World\n"
+
+s = open(file, "r")
+close(s)
+@test_throws Base.UVError Mmap.Array(s) # closed IOStream
+@test_throws ArgumentError Mmap.Array(s,12,0) # closed IOStream
+@test_throws SystemError Mmap.Array("")
+
+# negative length
+@test_throws ArgumentError Mmap.Array(file, -1)
+# negative offset
+@test_throws ArgumentError Mmap.Array(file, 1, -1)
+
+for i = 0x01:0x0c
+    @test length(Mmap.Array(file, i)) == Int(i)
+end
+gc()
+
+sz = filesize(file)
+m = Mmap.Array(file, sz+1)
+@test length(m) == sz+1 # test growing
+@test m[end] == 0x00
+m=nothing; gc()
+sz = filesize(file)
+m = Mmap.Array(file, 1, sz)
+@test length(m) == 1
+@test m[1] == 0x00
+m=nothing; gc()
+sz = filesize(file)
+# test where offset is actually > than size of file; file is grown with zeroed bytes
+m = Mmap.Array(file, 1, sz+1)
+@test length(m) == 1
+@test m[1] == 0x00
+m=nothing; gc()
+
+s = open(file, "r")
+m = Mmap.Array(s)
+@test_throws OutOfMemoryError m[5] = UInt8('x') # tries to setindex! on read-only array
+m=nothing; gc()
+
+s = open(file, "w") do f
+    write(f, "Hello World\n")
+end
+
+s = open(file, "r")
+m = Mmap.Array(s)
+close(s)
+m=nothing; gc()
+m = Mmap.Array(file)
+s = open(file, "r+")
+c = Mmap.Array(s)
+d = Mmap.Array(s)
+c[1] = UInt8('J')
+Mmap.sync!(c)
+close(s)
+@test m[1] == UInt8('J')
+@test d[1] == UInt8('J')
+m=nothing; c=nothing; d=nothing; gc()
+
+s = open(file, "w") do f
+    write(f, "Hello World\n")
+end
+
+s = open(file, "r")
+@test isreadonly(s) == true
+c = Mmap.Array(UInt8, (11,), s)
+@test c == "Hello World".data
+c=nothing; gc()
+c = Mmap.Array(UInt8, (UInt16(11),), s)
+@test c == "Hello World".data
+c=nothing; gc()
+@test_throws ArgumentError Mmap.Array(UInt8, (Int16(-11),),  s)
+@test_throws ArgumentError Mmap.Array(UInt8, (typemax(UInt),),  s)
+close(s)
+s = open(file, "r+")
+@test isreadonly(s) == false
+c = Mmap.Array(UInt8, (11,), s)
+c[5] = UInt8('x')
+Mmap.sync!(c)
+close(s)
+s = open(file, "r")
+str = readline(s)
+close(s)
+@test startswith(str, "Hellx World")
+c=nothing; gc()
+
+c = Mmap.Array(file)
+@test c == "Hellx World\n".data
+c=nothing; gc()
+c = Mmap.Array(file, 3)
+@test c == "Hel".data
+c=nothing; gc()
+s = open(file, "r")
+c = Mmap.Array(s, 6)
+@test c == "Hellx ".data
+close(s)
+c=nothing; gc()
+c = Mmap.Array(file, 5, 6)
+@test c == "World".data
+c=nothing; gc()
+
+s = open(file, "w")
+write(s, "Hello World\n")
+close(s)
+
+# test Mmap.Array
+m = Mmap.Array(file)
+t = "Hello World\n"
+for i = 1:12
+    @test m[i] == t.data[i]
+end
+@test_throws BoundsError m[13]
+m=nothing; gc()
+
+m = Mmap.Array(file,6)
+@test m[1] == "H".data[1]
+@test m[2] == "e".data[1]
+@test m[3] == "l".data[1]
+@test m[4] == "l".data[1]
+@test m[5] == "o".data[1]
+@test m[6] == " ".data[1]
+@test_throws BoundsError m[7]
+m=nothing; gc()
+
+m = Mmap.Array(file,2,6)
+@test m[1] == "W".data[1]
+@test m[2] == "o".data[1]
+@test_throws BoundsError m[3]
+m=nothing; gc()
+
+# mmap with an offset
+A = rand(1:20, 500, 300)
+fname = tempname()
+s = open(fname, "w+")
+write(s, size(A,1))
+write(s, size(A,2))
+write(s, A)
+close(s)
+s = open(fname)
+m = read(s, Int)
+n = read(s, Int)
+A2 = Mmap.Array(Int, (m,n), s)
+@test A == A2
+seek(s, 0)
+A3 = Mmap.Array(Int, (m,n), s, convert(FileOffset,2*sizeof(Int)))
+@test A == A3
+A4 = Mmap.Array(Int, (m,150), s, convert(FileOffset,(2+150*m)*sizeof(Int)))
+@test A[:, 151:end] == A4
+close(s)
+A2=nothing; A3=nothing; A4=nothing; gc()
+rm(fname)
+
+# AnonymousMmap
+m = Mmap.AnonymousMmap()
+@test m.name == ""
+@test !m.readonly
+@test m.create
+@test isopen(m)
+@test isreadable(m)
+@test iswritable(m)
+
+m = Mmap.Array(UInt8, 12)
+@test length(m) == 12
+@test all(m .== 0x00)
+@test m[1] === 0x00
+@test m[end] === 0x00
+m[1] = 0x0a
+Mmap.sync!(m)
+@test m[1] === 0x0a
+m = Mmap.Array(UInt8, 12; shared=false)
+m = Mmap.Array(Int, 12)
+@test length(m) == 12
+@test all(m .== 0)
+@test m[1] === 0
+@test m[end] === 0
+m = Mmap.Array(Float64, 12)
+@test length(m) == 12
+@test all(m .== 0.0)
+m = Mmap.Array(Int8, (12,12))
+@test size(m) == (12,12)
+@test all(m == zeros(Int8, (12,12)))
+@test sizeof(m) == prod((12,12))
+n = similar(m)
+@test size(n) == (12,12)
+n = similar(m, (2,2))
+@test size(n) == (2,2)
+n = similar(m, 12)
+@test length(n) == 12
+@test size(n) == (12,)
+
+n = similar(m, UInt8)
+@test size(n) == size(m)
+@test eltype(n) == UInt8
+m = Mmap.zeros(UInt8, (12,12))
+n = Mmap.zeros(UInt8, (12,12))
+@test n == m
